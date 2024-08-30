cd431890b039b7cf4c4bfe2c7225a31d9198a405
Sebastien_Oulevey <sebastien@ouleveysa.ch>
2024-08-30 16:42:57 +0200

    Limit the FPS

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import pygame
 from constants import *
 
-def boilerplate():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
 def main():
     pygame.init()
-    boilerplate()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0  
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == '__main__':
-    main()
+    main()  
